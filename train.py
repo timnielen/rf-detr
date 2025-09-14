19bf8a6a772c11d91a03f1e1cb048743f600a895
Tim <tim.nielen@online.de>
2025-09-14 02:52:33 +0200

    update

--- train.py
+++ train.py
@@ -1,17 +1,16 @@
 from rfdetr import RFDETRNano
 
-model = RFDETRNano()
+if __name__ == "__main__":
+    model = RFDETRNano()
 
-model.train(
-    dataset_dir="../playdarts/dataset",
-    epochs=10,
-    batch_size=4,
-    grad_accum_steps=4,
-    lr=1e-4,
-    output_dir="output_model",
-    wandb=False,
-    project="RFDETR",
-    run="train_0",
-    world_size=1,
-    num_workers=0,
-)
+    model.train(
+        dataset_dir="../playdarts/dataset",
+        epochs=15,
+        batch_size=20,
+        grad_accum_steps=1,
+        lr=1e-4,
+        output_dir="output2",
+        wandb=False,
+        num_workers=0,
+        # resume="output2/checkpoint.pth",
+    )
